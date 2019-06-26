c69066659dc2f6c077b0a30df0085f59535c732c
Ryan <ryangithub@gmail.com>
2019-06-26 17:55:14 -0500

    Started Project
    Added all the files unity adds.

---
+++ TetrisBot/Assets/MinoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.D)) {
+//			print ("D");
+			this.gameObject.transform.eulerAngles += new Vector3 (0, 0, -90);
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+//			print ("A");
+			this.gameObject.transform.eulerAngles += new Vector3 (0, 0, 90);
+		}
+	}
+}
---
+++ TetrisBot/Assets/Mino_Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mino_Spawner : MonoBehaviour {
+
+	public bool minoIsSpawned;
+	public GameObject[] minos;
+
+	// Use this for initialization
+	void Start () {
+//		minoIsSpawned = false;
+		Instantiate(minos[0], minos[0].transform.position, Quaternion.identity);
+		minoIsSpawned = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
